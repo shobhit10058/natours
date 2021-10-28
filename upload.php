6c6b5f83edf3da15b790f4f7313d724c76a12f71
Shobhit Gupta <56156171+shobhit10058@users.noreply.github.com>
2021-10-28 17:46:01 +0530

    added back in upload

--- upload.php
+++ upload.php
@@ -47,4 +47,19 @@ if ($uploadOk == 0) {
     echo "Sorry, there was an error uploading your file.";
   }
 }
+
+echo('
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body> 
+  <button><a href="admin_page.php" tite="Back">Back</button> 
+</body>
+</html>
+');
 ?>
